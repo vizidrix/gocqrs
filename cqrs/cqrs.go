21fc9024b621d29b67742b14ab41e019b39bce36
Richard Villa <rlpvilla@gmail.com>
2014-05-25 22:46:42 -0700

    Correlations

--- cqrs/cqrs.go
+++ cqrs/cqrs.go
@@ -65,14 +65,15 @@ type Command interface {
 
 type CommandMemento struct {
 	AggregateMemento        // Aggregate
-	CommandId        uint64 `json:"__cid"`   // Command Id
 	CommandType      uint32 `json:"__ctype"` // Command Type
+	CommandId        uint64 `json:"__cid"`   // Command Id
 }
 
-func NewCommand(domain uint32, commandType uint32, id uint64, version int32) CommandMemento {
+func NewCommand(domain uint32, aggregateid uint64, version int32, commandType uint32, commandid uint64) CommandMemento {
 	return CommandMemento{
-		AggregateMemento: NewAggregate(domain, id, version),
+		AggregateMemento: NewAggregate(domain, aggregateid, version),
 		CommandType:      commandType,
+		CommandId:        commandid,
 	}
 }
 
@@ -95,14 +96,15 @@ type Event interface {
 
 type EventMemento struct {
 	AggregateMemento        // Aggregate
-	EventId          uint64 `json:"__eid"`   // Event Id
 	EventType        uint32 `json:"__etype"` // Event Type
+	EventId          uint64 `json:"__eid"`   // Event Id
 }
 
-func NewEvent(domain uint32, eventType uint32, id uint64, version int32) EventMemento {
+func NewEvent(domain uint32, id uint64, version int32, eventType uint32, eventId uint64) EventMemento {
 	return EventMemento{
 		AggregateMemento: NewAggregate(domain, id, version),
 		EventType:        eventType,
+		EventId:          eventId,
 	}
 }
 
@@ -116,6 +118,24 @@ func (event EventMemento) GetEventId() uint64 {
 
 func (event EventMemento) String() string {
 	return fmt.Sprintf(" <E [ %s -> E[%d] ] E\\> ", event.AggregateMemento.String(), event.EventType)
+}
+
+type CorrelatedEvent interface {
+	GetCorrelationId() uint64
+}
+
+type CorrelationMemento struct {
+	CorrelationId uint64
+}
+
+func NewCorrelation(correlationId uint64) CorrelationMemento {
+	return CorrelationMemento{
+		CorrelationId: correlationId,
+	}
+}
+
+func (correlation CorrelationMemento) GetCorrelationId() uint64 {
+	return correlation.CorrelationId
 }
 
 type EventStorer interface {
@@ -144,7 +164,7 @@ func (eventstore *MemoryEventStore) StoreEvent(event Event) {
 func (eventstore *MemoryEventStore) ReadAllEvents() (int, []Event, error) {
 	return len(eventstore.Data), eventstore.Data, nil
 }
-
+c
 func (eventstore *MemoryEventStore) ReadAggregateEvents(domain uint32, id uint64) ([]Event, error) {
 	matching := make([]Event, 0)
 	for _, item := range eventstore.Data {
