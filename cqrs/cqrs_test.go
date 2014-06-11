a6821a2bda630cdf57beafc922cf4b06b00525f6
Richard Villa <rlpvilla@gmail.com>
2014-06-11 14:15:29 -0700

    Command and Event tests

--- cqrs/cqrs_test.go
+++ cqrs/cqrs_test.go
@@ -27,3 +27,13 @@ func Test_Should_calculate_correct_event_key(t *testing.T) {
 		t.Errorf("Expected [ %s ] but received [ %#x ]\n", expected, key)
 	}
 }
+
+func Test_Should_return_correct_domain(t *testing.T) {
+	var expected = "0x11111111"
+	var commandtype = C(TEST_DOMAIN, 1, 1)
+	var command = NewCommand(commandtype, 0, 0)
+
+	if fmt.Sprintf("%#x", command.GetDomain()) != expected {
+		t.Errorf("Expected [ %s ] but received [ %#x ]\n", expected, command.GetDomain())
+	}
+}
