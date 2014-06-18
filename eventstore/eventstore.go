5d8ad43b650c4e1b85d26a9f0c12c201e8a58a8d
Perry Birch <perrybirch@vizidrix.com>
2014-06-18 01:48:53 -0700

    Put logic update

--- eventstore/eventstore.go
+++ eventstore/eventstore.go
@@ -1,9 +1,14 @@
+// Eventstore provides interface and implementation used to store
+// and retrieve event streams related to domain aggregates
 package eventstore
 
 import (
-	//"fmt"
+	"fmt"
 	//"errors"
 	"os"
+	"bytes"
+	"encoding/binary"
+	"encoding/gob"
 	"github.com/boltdb/bolt"
 	"github.com/vizidrix/gocqrs/cqrs"
 )
@@ -37,10 +42,34 @@ func NewBoltES(
 	return
 }
 
+// Appends the provided event to the set by it's domain and aggregate id
 func (es *boltES) Put(event cqrs.Event) error {
-	return nil
+	err := es.DB.Update(func(tx *bolt.Tx) error {
+		domainKey := EncodeDomain(event.GetDomain())
+		bucket := tx.Bucket(domainKey)
+		if bucket == nil {
+			fmt.Printf("Nil bucket\n")
+			if _, err := tx.CreateBucketIfNotExists(domainKey); err != nil {
+				panic(fmt.Sprintf("Error creating bucket [ %s ]\n", err))
+			}
+			bucket = tx.Bucket(domainKey)
+			fmt.Printf("Made a bucket\n")
+		}
+		var buffer bytes.Buffer
+		enc := gob.NewEncoder(&buffer)
+		if err := enc.Encode(event); err != nil {
+			return err
+		}
+		fmt.Printf("Put Event Id: [ %d ] -> [ %v ]\n", event.GetId(), EncodeAggregateId(event.GetId()))
+		//var data []byte = make([]byte, 0, 0)
+		//EncodeAggregateId(event.GetId())
+		bucket.Put(EncodeAggregateId(event.GetId()), buffer.Bytes())//buffer.Bytes())
+		return nil
+		})
+	return err // Need to test retrieve of value and make data a slice of event
 }
 
+// Loads all events for the specified aggregate by it's id
 func (es *boltES) GetStreamById(domain uint32, id uint64) (<-chan cqrs.Event, error) {
 	stream := es.eventChanFactory() // Build an event chan to handle distribution
 	//make(chan cqrs.Event, 0) // Don't need buffered chan for no events
@@ -48,10 +77,26 @@ func (es *boltES) GetStreamById(domain uint32, id uint64) (<-chan cqrs.Event, er
 	return stream, nil
 }
 
+// Loads all events within the specified domain bucket
 func (es *boltES) GetStreamByDomain(domain uint32) (<-chan cqrs.Event, error) {
 	stream := es.eventChanFactory() // Build an event chan to handle distribution
 	return stream, nil
 }
+
+func EncodeDomain(domain uint32) []byte {
+	temp := make([]byte, 4, 4)
+	binary.BigEndian.PutUint32(temp, domain)
+	//fmt.Printf("Encoded domain [ %v ]\n", temp)
+	return temp
+}
+
+func EncodeAggregateId(aggregateId uint64) []byte {
+	temp := make([]byte, 8, 8)
+	binary.BigEndian.PutUint64(temp, aggregateId)
+	//fmt.Printf("Encoded aggregate id [ %v ]\n", temp)
+	return temp
+}
+
 /*
 var (
 	BUCKET_EVENTS []byte = []byte("events")
--- eventstore/eventstore_test.go
+++ eventstore/eventstore_test.go
@@ -116,6 +116,29 @@ func Test_Should_return_single_event_by_domain(t *testing.T) {
 }
 
 /*
+// This example transmits a value that implements the custom encoding and decoding methods.
+func main() {
+	var network bytes.Buffer // Stand-in for the network.
+
+	// Create an encoder and send a value.
+	enc := gob.NewEncoder(&network)
+	err := enc.Encode(Vector{3, 4, 5})
+	if err != nil {
+		log.Fatal("encode:", err)
+	}
+
+	// Create a decoder and receive a value.
+	dec := gob.NewDecoder(&network)
+	var v Vector
+	err = dec.Decode(&v)
+	if err != nil {
+		log.Fatal("decode:", err)
+	}
+	fmt.Println(v)
+
+}
+*/
+/*
 db, err := es.OpenEventStore("test.db", 0600)
 if err != nil {
 	return
