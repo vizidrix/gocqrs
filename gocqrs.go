592a8fb6653575444fb17c1c98de51031a2f9116
Richard Villa <rlpvilla@gmail.com>
2014-08-01 01:41:57 -0700

    refactored command handler framework

--- gocqrs.go
+++ gocqrs.go
@@ -10,7 +10,6 @@ var (
 	//ErrUsedTimestamp  = errors.New("timestamp used")
 	//ErrUsedKey        = errors.New("datastore key used")
 
-	
 	// ErrInvalidApplication is used to inform a consumer when they've
 	// provided an aggregate that doesn't have a valid application id
 	// that the receiving service is able to process
@@ -32,7 +31,7 @@ var (
 	// provided an aggregate with a version that cannot be sync'd
 	// with the current domain version
 	ErrInvalidVersion = errors.New("invalid aggregate version")
-	
+
 	// ErrInvalidCommandType is used to inform a consumer when they've
 	// provided a command type that isn't valid for the application and
 	// domain partition
@@ -92,13 +91,13 @@ func GetCommandTypeFromJson(jsonCommand []byte) (commandType uint64, err error) 
 // MakeVersionedCommandType provides a utility to union a command's version and
 // type identifiers and masks off the leftmost bit as 1 to indicate a command
 func MakeVersionedCommandType(version uint8, typeId uint32) uint32 {
-	return 0x80000000 | (uint32(version) << 24  & 0x7F000000) | (typeId & 0xFFFFFF)
+	return 0x80000000 | (uint32(version) << 24 & 0x7F000000) | (typeId & 0xFFFFFF)
 }
 
 // MakeVersionedEventType provides a utility to union an event's version and
 // type identifiers and masks off the leftmost bit as 0 to indicate an event
 func MakeVersionedEventType(version uint8, typeId uint32) uint32 {
-	return 0x7FFFFFFF & (uint32(version) << 24  & 0x7F000000) | (typeId & 0xFFFFFF)
+	return 0x7FFFFFFF&(uint32(version)<<24&0x7F000000) | (typeId & 0xFFFFFF)
 }
 
 // EventStoreReaderWriter describes a type the can be used to either read
@@ -127,6 +126,7 @@ type EventStoreReader interface {
 	LoadEventsByEventType(eventType uint32) ([]Event, error)
 	LoadEventsByEventTypes(eventTypes ...uint32) ([]Event, error)
 }
+
 /*
 type EventStoreReader interface {
 	LoadEventsByAggregate(application uint32, domain uint32, aggregate uint64) ([]Event, error)
@@ -155,18 +155,21 @@ type Command interface {
 
 // AggregateHydrator describes a method which processes a slice of events to produce
 // a populated aggregate instance
-type HydrateAggregate func([]Event)(Aggregate, error)
+type HydrateAggregate func([]Event) (Aggregate, error)
 
 // ApplyCommand describes a method which, given an aggregate instance and a command,
 // attempts to apply the command's intent then reports back success/fail event
-type ApplyCommand func(aggregate Aggregate, command Command)(Event, error)
+type ApplyCommand func(idgen AggregateIdGenerater, aggregate Aggregate, command Command) (Event, error)
 
 // PublishEvent describes a method which broadcasts an event to a pub/sub
-type PublishEvent func(event Event)(error)
+type PublishEvent func(event Event) error
 
 // CommandHandler describes a type that can be used to process commands
 type CommandHandler interface {
-	Handle(command Command) (error)
+	HydrateAggregate([]Event) (Aggregate, error)
+	ApplyCommand(Aggregate, Command) (Event, error)
+	PublishEvent(Event) error
+	Handle(command Command) error
 }
 
 // Event provides a base interface for all events in the system
@@ -216,14 +219,14 @@ type aggregate struct {
 	// at the application level partition for like domains within the same service
 	application uint32 `json:"_app"`
 	// domain is the type of aggregate (type is semantically equivalent to doman)
-	domain      uint32 `json:"_domain"`
+	domain uint32 `json:"_domain"`
 	// id is an [application / domain] unique identifier for the aggregate instance
 	// and should never be duplicated within that partition
-	id          uint64 `json:"_id"`
+	id uint64 `json:"_id"`
 	// version is derived from the number of events applied to the aggregate
 	// and provides guaranteed event ordering within it's
 	// [appliction / domain / id] partition
-	version     uint32 `json:"_ver"`
+	version uint32 `json:"_ver"`
 }
 
 // NewAggregate creates an aggregate instance with UUId derived from the provided values
@@ -267,21 +270,21 @@ type command struct {
 	// to the target aggregate by capturing the aggregate's full UUId
 	// partition information [ application / domain / id / version ]
 	aggregate
-	// commandType is an [ application / domain ] unique identifier for the type of 
+	// commandType is an [ application / domain ] unique identifier for the type of
 	// command message which captures the semantic intent of the command
-	commandType   uint32 `json:"_ctype"`
+	commandType uint32 `json:"_ctype"`
 }
 
 // NewCommand creates a command instance with UUID derived from the provided values
 // including the header of the targeted aggregate instance
 func NewCommand(application uint32, domain uint32, id uint64, version uint32, commandType uint32) Command {
-	return &command {
-		aggregate: aggregate {
+	return &command{
+		aggregate: aggregate{
 			application: application,
-			domain: domain,
-			id: id,
-			version: version,
-			},
+			domain:      domain,
+			id:          id,
+			version:     version,
+		},
 		commandType: commandType,
 	}
 }
@@ -295,35 +298,49 @@ func (command *command) GetCommandType() uint32 {
 // commandHandler is a container which wraps all the dependencies needed for
 // the general command handler case to perform all of it's related duties
 type commandHandler struct {
-	reader 		EventStoreReader
-	writer 		EventStoreWriter
-	hydrator 	HydrateAggregate
-	applicator 	ApplyCommand
-	publisher 	PublishEvent
+	reader      EventStoreReader
+	writer      EventStoreWriter
+	idGen       AggregateIdGenerater
+	hydrator    HydrateAggregate
+	applicator  ApplyCommand
+	publisher   PublishEvent
 	application uint32
-	domain 		uint32
+	domain      uint32
 }
 
 // NewCommandHandler provides a strongly typed list of dependencies needed to boot
 // a generalized command handler
 // Custom initializers could be built over this format to reduce the parameter list
-func NewCommandHandler(reader EventStoreReader, writer EventStoreWriter, hydrator HydrateAggregate, applicator ApplyCommand, publisher PublishEvent, application uint32, domain uint32) (CommandHandler) {
-	return &commandHandler {
-		reader: reader,
-		writer: writer,
-		hydrator: hydrator,
-		applicator: applicator,
-		publisher: publisher,
+func NewCommandHandler(reader EventStoreReader, writer EventStoreWriter, idgen AggregateIdGenerater, hydrator HydrateAggregate, applicator ApplyCommand, publisher PublishEvent, application uint32, domain uint32) CommandHandler {
+	return &commandHandler{
+		reader:      reader,
+		writer:      writer,
+		idGen:       idgen,
+		hydrator:    hydrator,
+		applicator:  applicator,
+		publisher:   publisher,
 		application: application,
-		domain: domain,
+		domain:      domain,
 	}
+}
+
+func (c *commandHandler) hydrateAggregate(events []Event) (Aggregate, error) {
+	return c.hydrator(events)
+}
+
+func (c *commandHandler) applyCommand(aggregate Aggregate, command Command) (Event, error) {
+	return c.applicator(c.idGen, aggregate, command)
+}
+
+func (c *commandHandler) publishEvent(event Event) error {
+	return c.publisher(event)
 }
 
 // Handle processes the provided command throgh the necessary steps to validate
 // the command, load the events from the store, populate the aggregate, apply
 // the command, append the resulting event and, finally, publish the event
 // TODO: Create an async Handle option
-func (c *commandHandler) Handle(command Command) (error) {
+func (c *commandHandler) Handle(command Command) error {
 	// Validate the partition of the command
 	if c.application != command.GetApplication() {
 		return ErrInvalidApplication
@@ -337,12 +354,12 @@ func (c *commandHandler) Handle(command Command) (error) {
 		return ErrUnableToFindAggregate
 	}
 	// Populate an aggregate using the retrieved events
-	aggregate, err := c.hydrator(events)
+	aggregate, err := c.hydrateAggregate(events)
 	if err != nil {
 		return ErrUnableToLoadAggregate
 	}
 	// Evaluate the command against the aggregate
-	event, err := c.applicator(aggregate, command)
+	event, err := c.applyCommand(aggregate, command)
 	if err != nil {
 		return ErrErrorApplyingCommand
 	}
@@ -352,7 +369,7 @@ func (c *commandHandler) Handle(command Command) (error) {
 		return ErrErrorAppendingEvent
 	}
 	// Broadcast the created event to all observers
-	err = c.publisher(event)
+	err = c.publishEvent(event)
 	if err != nil {
 		return ErrErrorPublishingEvent
 	}
@@ -365,21 +382,21 @@ type event struct {
 	// to the target aggregate by capturing the aggregate's full UUId
 	// partition information [ application / domain / id / version ]
 	aggregate
-	// eventType is an [ application / domain ] unique identifier for the type of 
+	// eventType is an [ application / domain ] unique identifier for the type of
 	// event message which captures the semantic intent of the event
-	eventType   uint32 `json:"_etype"`
+	eventType uint32 `json:"_etype"`
 }
 
 // NewEvent creates an event instance with UUID derived from the provided values
 // including the header of the targeted aggregate instance
 func NewEvent(application uint32, domain uint32, id uint64, version uint32, eventType uint32) Event {
 	return &event{
-		aggregate: aggregate {
+		aggregate: aggregate{
 			application: application,
-			domain: domain,
-			id: id,
-			version: version,
-			},
+			domain:      domain,
+			id:          id,
+			version:     version,
+		},
 		eventType: eventType,
 	}
 }
